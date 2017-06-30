55a7ee1f97c411b4a097e02d7b4bfd69116229e1
Jiming Wang (王继明) <Jiming.Wang@mediatek.com>
2017-06-30 10:16:20 +0800

    add shell function.sh

---
+++ www.runoob.com/funciton.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#author: jiming.wang
+#url: www.runoob.com
+
+demoFun()
+{
+	echo "this is my first shell function!"
+}
+
+echo "-----start function-----"
+demoFun
+echo "-----end function-----"
